741bf07b63063e15f4099251fea5733c59e71f36
Adarsh-MR <adarshmr1998@gmail.com>
2018-01-15 09:33:53 +0000

    op3: build with oppo/common
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- device.mk
+++ device.mk
@@ -312,3 +312,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
+# Inherit from oppo-common
+$(call inherit-product, device/oppo/common/common.mk)
